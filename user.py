bd3cb4499af58815a28a45870960be5b9447d484
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-04 23:46:21 +0300

    Add method and user list to class User

--- user.py
+++ user.py
@@ -2,3 +2,10 @@ class User:
     """
     Class that generates new instance of User
     """
+    user_list = []
+
+    def __init__(self, user_name, password):
+
+        self.user_name = user_name
+        self.password = password
+          

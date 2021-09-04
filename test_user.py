9ddf60465e39acf6651a34f585356489d7417707
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-04 23:50:25 +0300

    Add class credentials

---
+++ test_user.py
@@ -0,0 +1,1 @@
+import unittest #importing the unittest module
--- user.py
+++ user.py
@@ -8,4 +8,19 @@ class User:
 
         self.user_name = user_name
         self.password = password
-          
+    def save_user(self):
+
+        '''
+        save_contact method saves contact objects into contact_list
+        '''
+
+        User.user_list.append(self)
+
+    @classmethod
+    def display_user(cls):
+        '''
+        method that returns the user list
+        '''
+        return cls.user_list
+class  Credential:
+    

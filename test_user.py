d1f9e92c2cefa139dc0b4b01e42bbb6d12e8e047
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-05 00:01:55 +0300

    Modify test_user file

--- test_user.py
+++ test_user.py
@@ -1,7 +1,7 @@
 import unittest #importing the unittest module
 from user import Credential, User
 
-class TestContact(unittest.TestCase):
+class TestUser(unittest.TestCase):
 
     '''
     Test class that defines test cases for the User class behaviours.

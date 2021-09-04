1942762da0ec9f757a9a293fd4752b2311d1fe59
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-05 00:01:06 +0300

    Add class TestUser

--- test_user.py
+++ test_user.py
@@ -1,1 +1,9 @@
 import unittest #importing the unittest module
+from user import Credential, User
+
+class TestContact(unittest.TestCase):
+
+    '''
+    Test class that defines test cases for the User class behaviours.
+ 
+    '''

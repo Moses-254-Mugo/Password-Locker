843fb402798c6608261e2cb31b586e1240dbb7d5
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-05 15:52:42 +0300

    Add method save user to class  User

--- folder/user.py
+++ folder/user.py
@@ -7,3 +7,14 @@ class User:
     def __init__ (self, user_name, password):
         self.user_name = user_name
         self.password = password
+
+    def save_ser(self):
+        '''
+        method that returns the user_list
+        '''
+        User.user_list.append(self)
+
+    def save_user(self):
+        '''
+        '''
+        User.user_list.append()

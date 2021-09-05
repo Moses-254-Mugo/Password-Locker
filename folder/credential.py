77a19301201f005b88bcec5aeaf6abdeb70c5a52
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-05 17:13:07 +0300

    Add classmethod to verify user

--- folder/credential.py
+++ folder/credential.py
@@ -6,3 +6,18 @@ class Credentials:
     '''
     Test class the defines test cases form User class behaviour.
     '''
+
+    credential_list = []
+
+    def __init__(self, account, user_name, password):
+        self.account = account
+        self.user_name = user_name
+        self.password = password
+
+    @classmethod
+    def verify_user(cls, user_name, password):
+        verify_user = ''
+        for user in User.user_list:
+            if user.user_name == user_name and user.password == password:
+                verify_user = user.user_name
+            return verify_user

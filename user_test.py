4f9b8fecd32eb22f6f247997839b4aa36ac6926f
Moses-254-Mugo <moses.mugo@student.moringaschool.com>
2021-09-05 18:06:39 +0300

    Add class UserTest

--- user_test.py
+++ user_test.py
@@ -1,3 +1,8 @@
 import unittest
+from folder import user
 from folder.user import User
 from  folder.credential import Credentials
+
+class UserTest(unittest.TestCase):
+    def setUp(self):
+        self.user_list = user('moses', 'password')
